3ebf69e2a6e0079614885f93213e633a4cbda7e8
rh01 <1048157315@qq.com>
2019-03-25 20:54:16 +0800

    add random quicksort algo.

--- sort/mergesort/sort_test.go
+++ sort/mergesort/sort_test.go
@@ -29,7 +29,7 @@ func TestMergeSort(t *testing.T) {
 
 func BenchmarkMergeSort(b *testing.B) {
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
-	array1 := make([]int, random.Intn(1000000-10)+10)
+	array1 := make([]int, random.Intn(100-10)+10)
 	for i := range array1 {
 		array1[i] = random.Intn(100)
 	}
--- sort/randomquicksort/sort_test.go
+++ sort/randomquicksort/sort_test.go
@@ -1,4 +1,4 @@
-package quicksort
+package randomquicksort
 
 import (
 	"math/rand"
@@ -15,22 +15,21 @@ func TestQuickSort(t *testing.T) {
 	}
 	array2 := make(sort.IntSlice, len(array1))
 	copy(array2, array1)
-	QuickSort(array1, 0, len(array1)-1)
+	RandomQuickSort(array1, 0, len(array1)-1)
 	array2.Sort()
 	for i := range array1 {
 		if array1[i] != array2[i] {
 			t.Fail()
 		}
 	}
-
 }
 
-func BenchmarkTestQuickSort(b *testing.B) {
+func BenchmarkTestRandomQuickSort(b *testing.B) {
 	random := rand.New(rand.NewSource(time.Now().UnixNano()))
 	array1 := make([]int, random.Intn(100-10)+10)
 	for i := range array1 {
 		array1[i] = random.Intn(100)
 	}
 	b.ResetTimer()
-	QuickSort(array1, 0, len(array1)-1)
+	RandomQuickSort(array1, 0, len(array1)-1)
 }
